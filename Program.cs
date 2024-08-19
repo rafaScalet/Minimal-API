77e56bc4ec70c6e1339fb6645385fde94b27ed61
rafaScalet <rafaelscalet2018@gmail.com>
2024-08-18 21:03:54 -0300

    first DTO

--- Program.cs
+++ Program.cs
@@ -3,4 +3,17 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDTO loginDTO) => {
+	if (loginDTO.Email == "admin@email.com" && loginDTO.PWD == "1234")
+		return Results.Ok("Login efetuado");
+	else
+		return Results.Unauthorized();
+});
+
 app.Run();
+
+public class LoginDTO
+{
+	public string Email { get; set; } = default!;
+	public string PWD { get; set; } = default!;
+}
