6c718b3a0cde5ebcd3bd85375fe7396e8ab84597
rafaScalet <rafaelscalet2018@gmail.com>
2024-08-19 09:48:05 -0300

    PWD and Profile is now required

--- Domain/Entities/Admin.cs
+++ Domain/Entities/Admin.cs
@@ -13,9 +13,11 @@ public class Admin
 	[StringLength(255)]
 	public string Email { get; set; } = default!;
 
+	[Required]
 	[StringLength(50)]
 	public string PWD { get; set; } = default!;
 
+	[Required]
 	[StringLength(10)]
 	public string Profile { get; set; } = default!;
 }
