b3d4a59365c38489bb6c80312070689b9166c31c
rafaScalet <rafaelscalet2018@gmail.com>
2024-08-19 09:52:48 -0300

    Add class Vehicles

---
+++ Domain/Entities/Vehicles.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinimalAPI.Entities.Admin;
+
+public class Vehicles
+{
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int Id { get; set; } = default!;
+
+	[Required]
+	[StringLength(150)]
+	public string Name { get; set; } = default!;
+
+	[Required]
+	[StringLength(100)]
+	public string Mark { get; set; } = default!;
+
+	[Required]
+	public int Year { get; set; } = default!;
+}
