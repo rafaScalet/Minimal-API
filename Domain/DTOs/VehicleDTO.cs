c22b6789066c293dc7b28bced3fe2454cc4b9053
rafaScalet <rafaelscalet2018@gmail.com>
2024-10-09 17:16:58 -0300

    refactor

---
+++ Domain/DTOs/VehicleDTO.cs
@@ -0,0 +1,8 @@
+namespace MinimalAPI.Domain.DTOs;
+
+public class VehicleDTO
+{
+	public int year { get; set; } = default!;
+	public string mark { get; set; } = default!;
+	public string name { get; set; } = default!;
+}
